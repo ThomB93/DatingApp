7bec479c2c017a04d2c4dc915913551356441f1d
Thomas Brunbjerg <thomas.brunbjerg@dgi.dk>
2021-11-23 12:02:39 +0100

    Add project files.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DataContext() : base()
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
